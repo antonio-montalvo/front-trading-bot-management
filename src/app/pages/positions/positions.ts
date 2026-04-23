44b36322c4f0c55c1d5c8e045abf9a1e18bd28fa
Antonio <ant.mon.diaz@hotmail.com>
2026-04-23 13:40:53 -0600

    Add Angular project configuration and initial application structure

---
+++ src/app/pages/positions/positions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-positions',
+  imports: [],
+  templateUrl: './positions.html',
+  styleUrl: './positions.scss'
+})
+export class Positions {}
